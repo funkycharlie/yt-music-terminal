579a3cfad18a8c9520b700f2caee816748597013
Charlie <funkycharlie4@gmail.com>
2024-01-22 18:47:20 +0000

    username but fancier

--- main.py
+++ main.py
@@ -4,20 +4,33 @@ import os
 import time
 import sys
 import json
+import curses
+from curses import wrapper
+from curses.textpad import Textbox
 
 
-def main():
-    if not os.path.exists("oauth.json"):
-        print("""Hi there, looks like it's your first time, or your log in expired.
+
+# YT OAuth Workflow
+if not os.path.exists("oauth.json"):
+    print("""Hi there, looks like it's your first time, or your log in expired.
 You'll be redirected to Google's website to log in, and then we can get started!""")
-        time.sleep(3)
-        try:
-            subprocess.run(["ytmusicapi", "oauth"])
-        except Exception:
-            print("Sorry, we couldn't log you in. Make sure you installed ytmusicapi.")
-            sys.exit()
+    time.sleep(3)
+    try:
+        subprocess.run(["ytmusicapi", "oauth"])
+    except:
+        print("Sorry, we couldn't log you in. Make sure you installed ytmusicapi.")
+        sys.exit()
 
+
+def main(window):
+
+    # Init de YT Music
     yt = YTMusic("oauth.json")
+
+    # Init de curses
+    curses.noecho()
+    curses.cbreak()
+    window.keypad(True)
 
     def get_user_details():
         try:
@@ -25,15 +38,22 @@ You'll be redirected to Google's website to log in, and then we can get started!
                 user_data = json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             user_data = {}
-            user_name = input("I don't think we've met. What's your name? ")
+            h, w = window.getmaxyx()
+            text = "I don't think we've met. What's your name?"
+            x = w // 2 - len(text) // 2
+            y = h // 2
+            window.addstr(y, x, text, curses.A_STANDOUT)
+            namewin = curses.newwin(1, len(text), y + 4, x)
+            window.refresh()
+            box = Textbox(namewin)
+            box.edit()
+            user_name = box.gather()
             user_data['name'] = user_name
             with open('user_data.json', 'w') as file:
                 json.dump(user_data, file)
         return user_data
 
     user_data = get_user_details()
-    print(f"Hey there, {user_data['name']}! Ready for some music?")
 
 
-if __name__ == "__main__":
-    main()
+wrapper(main)
